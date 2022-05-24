9a8feb00d5dc6ad51ca7af90bc55d191631c7916
Yoann Brambilla <yobrambilla@gmail.com>
2022-05-24 15:44:52 +0200

    doctrine first for TIC

---
+++ src/Repository/SquirellRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Squirell;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Squirell>
+ *
+ * @method Squirell|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Squirell|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Squirell[]    findAll()
+ * @method Squirell[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SquirellRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Squirell::class);
+    }
+
+    public function add(Squirell $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Squirell $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Squirell[] Returns an array of Squirell objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('s')
+//            ->andWhere('s.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('s.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Squirell
+//    {
+//        return $this->createQueryBuilder('s')
+//            ->andWhere('s.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
